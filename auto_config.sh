a6cfa06926f4bbaf62fdfad38b1ea2d05155d331
Skylar Iolta <Skylar.Iolta@revolutionanalytics.com>
2015-02-24 13:21:58 -0800

    Small test commit

--- auto_config.sh
+++ auto_config.sh
@@ -3,7 +3,9 @@
 # This script will copy the appropriate configuration files to their respective locations to ensure a homogenious environment
 
 if [[ `cat /etc/issue` == *CentOS* ]]; then
-	
+
 
 elif [[ `cat /etc/issue` == *Ubuntu* ]]; then
 	
+
+# test for test commit
