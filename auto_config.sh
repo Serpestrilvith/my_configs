ada03d0c692d8d529934bd09493a88861afbb45f
Skylar Iolta <Skylar.Iolta@revolutionanalytics.com>
2015-02-24 14:48:26 -0800

    Added zsh install command.

--- auto_config.sh
+++ auto_config.sh
@@ -3,9 +3,9 @@
 # This script will copy the appropriate configuration files to their respective locations to ensure a homogenious environment
 
 if [[ `cat /etc/issue` == *CentOS* ]]; then
-
+	sudo yum update -y
 
 elif [[ `cat /etc/issue` == *Ubuntu* ]]; then
-	
+	sudo apt-get update && sudo apt-get upgrade
 
-# test for test commit
+sudo wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sh
