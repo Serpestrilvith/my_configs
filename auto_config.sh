c4c23ce1ddf4274f59c3d9e5b7511ba8c60b377f
Skylar Iolta <Skylar.Iolta@revolutionanalytics.com>
2015-02-24 12:52:53 -0800

    Added if statements to handle os variants

--- auto_config.sh
+++ auto_config.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
 # This script will copy the appropriate configuration files to their respective locations to ensure a homogenious environment
+
+if [[ `cat /etc/issue` == *CentOS* ]]; then
+	
+
+elif [[ `cat /etc/issue` == *Ubuntu* ]]; then
+	
