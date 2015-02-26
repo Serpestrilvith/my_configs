83b3867e5a7134aaf4ed82907198857431789f1a
Skylar Iolta <Skylar.Iolta@revolutionanalytics.com>
2015-02-25 16:32:42 -0800

    Added progress visualization.

--- auto_config.sh
+++ auto_config.sh
@@ -4,14 +4,16 @@
 
 if [[ `uname -a` == *Darwin* ]]; then
 	echo "Installing brew and zsh..."
-	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" #> /dev/null 2>&1
-	brew install wget zsh -y #> /dev/null 2>&1
+	while true;do echo -n .;sleep 1;done &
+	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" > /dev/null 2>&1
+	brew install wget zsh -y > /dev/null 2>&1
 elif [[ `cat /etc/issue` == *CentOS* ]]; then
 	echo "Updating packages and installing zsh..."
 	sudo yum update -y > /dev/null 2>&1
 	sudo yum install zsh -y > /dev/null 2>&1
 elif [[ `cat /etc/issue` == *Ubuntu* ]]; then
 	echo "Updating packages and installing zsh..."
+	while true;do echo -n .;sleep 1;done &
 	sudo apt-get update > /dev/null 2>&1 && sudo apt-get upgrade -y
 	sudo apt-get install zsh -y > /dev/null 2>&1
 fi
@@ -29,4 +31,6 @@ if [[ `echo $SHELL` != *zsh* ]]; then
 	chsh -s $(which zsh)
 fi
 
-echo "Done!"
+kill $!; trap 'kill $!' SIGTERM
+DONE=$'\nDone!'
+echo "$DONE"
