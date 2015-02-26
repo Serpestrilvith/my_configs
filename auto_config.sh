b36898d88adcdee97604e47d7f6541e7ef37497d
Skylar Iolta <Skylar.Iolta@revolutionanalytics.com>
2015-02-25 16:09:25 -0800

    Added OSX section.

--- auto_config.sh
+++ auto_config.sh
@@ -10,17 +10,20 @@ elif [[ `cat /etc/issue` == *Ubuntu* ]]; then
 	echo "Updating packages and installing zsh..."
 	sudo apt-get update > /dev/null 2>&1 && sudo apt-get upgrade -y
 	sudo apt-get install zsh -y > /dev/null 2>&1
+elif [[ `uname -a` == *Darwin* ]]; then
+	echo "Installing brew and zsh..."
+	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	brew install wget zsh -y
 fi
 
 # Check if zsh is installed
 if [ ! -d ~/.oh-my-zsh ]; then
 	sudo wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O - | sh
+	cd ~/.oh-my-zsh/custom/plugins
+	git clone git://github.com/zsh-users/zsh-syntax-highlighting.git
 fi
 
 rm -f ~/.zshrc && cp ./.zshrc ~/
-
-cd ~/.oh-my-zsh/custom/plugins
-git clone git://github.com/zsh-users/zsh-syntax-highlighting.git
 
 if [[ `echo $SHELL` != *zsh* ]]; then
 	chsh -s $(which zsh)
